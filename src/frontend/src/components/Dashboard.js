cbd383c248a5d3ac75b4e026bdc3a66a57f9f8fe
Artem Ishchenko <artem.ischchenko@gmail.com>
2024-12-19 20:10:03 +0200

    update chart with critical value

--- src/frontend/src/components/Dashboard.js
+++ src/frontend/src/components/Dashboard.js
@@ -1,12 +1,22 @@
 // src/components/Dashboard.js
-import React from "react";
+import React, { useState } from 'react';
 import SensorDataChart from "./SensorDataChart";
+import DeviceDropdown from "./DeviceDropdown";
 
 const Dashboard = () => {
+    const [selectedDevice, setSelectedDevice] = useState({ id: null, criticalValue: null });
+
+    const handleDeviceSelection = (deviceId, criticalValue) => {
+        setSelectedDevice({ id: deviceId, criticalValue });
+    };
+
     return (
         <div className="container">
             <h2>Welcome to the Dashboard</h2>
-        <SensorDataChart deviceId={1}></SensorDataChart>
+            <DeviceDropdown onDeviceSelect={handleDeviceSelection} />
+            {selectedDevice && (
+                <SensorDataChart deviceId={selectedDevice.id} criticalValue={selectedDevice.criticalValue} />
+            )}
         </div>
     );
 };
--- src/frontend/src/components/SensorDataChart.js
+++ src/frontend/src/components/SensorDataChart.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Line } from "react-chartjs-2";
+import React, {useEffect, useState} from "react";
+import {Line} from "react-chartjs-2";
 import axios from "axios";
 import {
     Chart as ChartJS,
@@ -14,10 +14,32 @@ import {
 
 ChartJS.register(LineElement, PointElement, CategoryScale, LinearScale, Tooltip, Legend, Title);
 
-const SensorDataChart = ({ deviceId }) => {
+const SensorDataChart = ({deviceId, criticalValue}) => {
     const [chartData, setChartData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+
+    const criticalValuePlugin = {
+        id: "criticalValueLine",
+        beforeDraw: (chart) => {
+            if (criticalValue === null || !chart || !chart.scales) return;
+
+            const {ctx, scales} = chart;
+            const yAxis = scales.y;
+            const xAxis = scales.x;
+
+            const yPosition = yAxis.getPixelForValue(criticalValue);
+
+            ctx.save();
+            ctx.beginPath();
+            ctx.moveTo(xAxis.left, yPosition);
+            ctx.lineTo(xAxis.right, yPosition);
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "red";
+            ctx.stroke();
+            ctx.restore();
+        },
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -28,31 +50,33 @@ const SensorDataChart = ({ deviceId }) => {
                 twoWeeksAgo.setDate(now.getDate() - 90);
 
                 // Fetch data from backend API
-                const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/metrics`, {
-                    params: {
-                        deviceId: deviceId,
-                        start: twoWeeksAgo.toISOString(),
-                        end: now.toISOString(),
-                    },
-                    withCredentials: true
-                });
-
-                // Transform data for the chart
-                const timestamps = response.data.map((entry) => entry.timestamp);
-                const values = response.data.map((entry) => entry.value);
-
-                setChartData({
-                    labels: timestamps,
-                    datasets: [
-                        {
-                            label: "Sensor Values",
-                            data: values,
-                            borderColor: "rgba(75,192,192,1)",
-                            backgroundColor: "rgba(75,192,192,0.2)",
-                            tension: 0.1,
+                if (deviceId) {
+                    const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/metrics`, {
+                        params: {
+                            deviceId: deviceId,
+                            start: twoWeeksAgo.toISOString(),
+                            end: now.toISOString(),
                         },
-                    ],
-                });
+                        withCredentials: true
+                    });
+
+                    // Transform data for the chart
+                    const timestamps = response.data.map((entry) => entry.timestamp);
+                    const values = response.data.map((entry) => entry.value);
+
+                    setChartData({
+                        labels: timestamps,
+                        datasets: [
+                            {
+                                label: "Sensor Values",
+                                data: values,
+                                borderColor: "rgba(75,192,192,1)",
+                                backgroundColor: "rgba(75,192,192,0.2)",
+                                tension: 0.1,
+                            },
+                        ],
+                    });
+                }
             } catch (err) {
                 // TODO: invalidate session in case of 401 and redirect to login
                 setError("Failed to fetch data");
@@ -70,7 +94,8 @@ const SensorDataChart = ({ deviceId }) => {
     return (
         <div>
             <h2>Sensor Data for Last Two Weeks</h2>
-            {chartData && <Line data={chartData} options={{ responsive: true }} />}
+            <h2>{criticalValue}</h2>
+            {chartData && <Line data={chartData} options={{responsive: true}} plugins={[criticalValuePlugin]}/>}
         </div>
     );
 };
