93f25d0ae717d2b88ac98cc340ecd3a42e381f71
Artem Ishchenko <artem.ischchenko@gmail.com>
2025-04-07 00:24:41 +0300

    update menu

--- src/frontend/src/components/menu/Menu.js
+++ src/frontend/src/components/menu/Menu.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaHome, FaCogs, FaUsers } from 'react-icons/fa';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {FaHome, FaCogs, FaUsers, FaBars} from 'react-icons/fa';
 import './Menu.css';
 
 const Menu = () => {
-    return (
-        <nav className="vertical-menu">
-            <ul>
-                <li><Link to="/dashboard"><FaHome /> Main</Link></li>
-                <li><Link to="/devices"><FaCogs /> Devices</Link></li>
-                <li><Link to="/users"><FaUsers /> Users</Link></li>
-            </ul>
-        </nav>
-    );
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (<>
+            <div className="navbar">
+                <FaBars className="burger-button" onClick={toggleMenu}/>
+                <div className="title">My Application</div>
+            </div>
+
+            <nav className={`vertical-menu ${isOpen ? 'open' : ''}`}>
+                <ul>
+                    <li><Link to="/dashboard" onClick={toggleMenu}><FaHome/><span> Main</span></Link></li>
+                    <li><Link to="/devices" onClick={toggleMenu}><FaCogs/><span>Devices</span></Link></li>
+                    <li><Link to="/users" onClick={toggleMenu}><FaUsers/><span>Users</span></Link></li>
+                </ul>
+            </nav>
+        </>);
 };
 
 export default Menu;
