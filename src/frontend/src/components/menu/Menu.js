a3651d88da71dca4c25fd4f67da26c5eee454aa3
Artem Ishchenko <artem.ischchenko@gmail.com>
2025-04-07 23:03:39 +0300

    add logo

--- src/frontend/src/components/menu/Menu.js
+++ src/frontend/src/components/menu/Menu.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {FaHome, FaCogs, FaUsers, FaBars} from 'react-icons/fa';
 import './Menu.css';
+import Logo from "../logo/Logo";
 
 const Menu = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,7 +14,7 @@ const Menu = () => {
     return (<>
             <div className="navbar">
                 <FaBars className="burger-button" onClick={toggleMenu}/>
-                <div className="title">My Application</div>
+                <Logo /><div className="title">Smart Sensor Network</div>
             </div>
 
             <nav className={`vertical-menu ${isOpen ? 'open' : ''}`}>
