5998c5a3d38887eb0748b059b47a5100cb39d7e7
Artem Ishchenko <artem.ischchenko@gmail.com>
2025-12-05 12:12:18 +0200

    fix update role issue

--- src/main/java/org/proteus1121/service/UserDeviceService.java
+++ src/main/java/org/proteus1121/service/UserDeviceService.java
@@ -12,10 +12,7 @@ import org.proteus1121.model.mapper.UserMapper;
 import org.proteus1121.repository.UserDeviceRepository;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.parser.Entity;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -27,21 +24,27 @@ public class UserDeviceService {
     private final UserMapper userMapper;
     private final UserDeviceRepository userDeviceRepository;
     private final EntityManager entityManager;
-    
+
     @Transactional
     public Set<UserDevices> shareDevice(Long deviceId, Map<Long, DeviceRole> users) {
 
         return users.entrySet().stream()
                 .map(entry -> userDeviceRepository.findByUserIdAndDeviceId(entry.getKey(), deviceId)
-                        .orElseGet(() -> userDeviceRepository.saveAndFlush(
-                                userDeviceMapper.toLinkByIds(entry.getKey(), deviceId, entry.getValue())
-                        ))
+                        .map(deviceEntity -> {
+                            if (deviceEntity.getRole() == entry.getValue()) {
+                                return deviceEntity;
+                            }
+                            deviceEntity.setRole(entry.getValue());
+                            return userDeviceRepository.save(deviceEntity);
+                        })
+                        .orElseGet(() -> userDeviceRepository.save(userDeviceMapper.toLinkByIds(entry.getKey(), deviceId, entry.getValue())))
                 )
+                // TODO: remove refresh after fixing N+1 problem with UserDeviceEntity->UserEntity mapping
                 .peek(entityManager::refresh)
                 .map(userDeviceMapper::toDeviceUser)
                 .collect(Collectors.toSet());
     }
-    
+
     public void unshareDevice(Long deviceId, Long userId) {
         userDeviceRepository.deleteByUserIdAndDeviceId(userId, deviceId);
     }
